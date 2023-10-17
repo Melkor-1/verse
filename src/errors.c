ae53f91ae4057e3cdc80bc8099cb25cfe43a681d
Melkor-1 <harith.bin.salam@gmail.com>
2023-10-17 04:14:05 -0700

    Fixed indentation.

--- src/errors.c
+++ src/errors.c
@@ -2,12 +2,12 @@
 
 #include <assert.h>
 
-const char *get_err_msg (int err_code)
+const char *get_err_msg(int err_code)
 {
-    static_assert (ARRAY_CARDINALITY (errors) - 1 == E_CURL_PERFORM_FAILED,
-                   "The errors array and the enum must be kept in-sync!");
+    static_assert(ARRAY_CARDINALITY(errors) - 1 == E_CURL_PERFORM_FAILED,
+                  "The errors array and the enum must be kept in-sync!");
 
-    if (err_code >= 0 && err_code < (int) ARRAY_CARDINALITY (errors)) {
+    if (err_code >= 0 && err_code < (int) ARRAY_CARDINALITY(errors)) {
         return errors[err_code];
     }
     return "Unknown error code.\n";
--- src/web_util.c
+++ src/web_util.c
@@ -1,5 +1,5 @@
 #ifndef _XOPEN_SOURCE
-#define _XOPEN_SOURCE 700
+#    define _XOPEN_SOURCE 700
 #endif
 
 #include "web_util.h"
@@ -13,14 +13,15 @@
 
 #define MAX_VERSE_SIZE 4096
 
-int
-parse_response_json (const char *restrict json_response, char **restrict out)
+int parse_response_json(const char *restrict json_response, char **restrict out)
 {
     json_error_t error;
     json_t *root = json_loads(json_response, 0, &error);
 
-    if(root) {
-        /* Check if "text" exists in the JSON structure. */
+    if (root) {
+        /*
+         * Check if "text" exists in the JSON structure. 
+         */
         const json_t *const data = json_object_get(root, "data");
         const json_t *const text = json_object_get(data, "text");
 
@@ -29,7 +30,7 @@ parse_response_json (const char *restrict json_response, char **restrict out)
         } else {
             json_decref(root);
             return E_UNKNOWN;
-        }    
+        }
     } else {
         fputs(error.text, stderr);
         return E_UNKNOWN;
@@ -40,52 +41,57 @@ parse_response_json (const char *restrict json_response, char **restrict out)
 }
 
 size_t
-write_memory_callback (void *content, size_t size,
-                       size_t nmemb, struct mem_chunk *chunk)
+write_memory_callback(void *content, size_t size,
+                      size_t nmemb, struct mem_chunk *chunk)
 {
     const size_t new_size = chunk->len + size * nmemb;
-    void *const cp = realloc (chunk->ptr, new_size + 1);
+    void *const cp = realloc(chunk->ptr, new_size + 1);
 
     if (!cp) {
-        perror ("realloc()");
+        perror("realloc()");
         return 0;
     }
     chunk->ptr = cp;
-    memcpy (chunk->ptr + chunk->len, content, size * nmemb);
+    memcpy(chunk->ptr + chunk->len, content, size * nmemb);
     chunk->ptr[new_size] = '\0';
     chunk->len = new_size;
     return size * nmemb;
 }
 
 int
-download_webpage (const struct mem_chunk *restrict chunk,
-                  CURL * restrict curl, const char *restrict url)
+download_webpage(const struct mem_chunk *restrict chunk,
+                 CURL * restrict curl, const char *restrict url)
 {
-    CURLcode ret; 
+    CURLcode ret;
 
-    if ((ret = curl_easy_setopt (curl, CURLOPT_URL, url)) != CURLE_OK 
-        || (ret = curl_easy_setopt (curl, CURLOPT_WRITEFUNCTION, write_memory_callback)) != CURLE_OK
-        || (ret = curl_easy_setopt (curl, CURLOPT_WRITEDATA, chunk)) != CURLE_OK
-        || (ret = curl_easy_setopt (curl, CURLOPT_USERAGENT, "Verse/1.0")) != CURLE_OK
-        || (ret = curl_easy_setopt (curl, CURLOPT_FOLLOWLOCATION, 1L)) != CURLE_OK
-        || (ret = curl_easy_setopt (curl, CURLOPT_MAXREDIRS, 5L)) != CURLE_OK) {
+    if ((ret = curl_easy_setopt(curl, CURLOPT_URL, url)) != CURLE_OK
+        || (ret =
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION,
+                             write_memory_callback)) != CURLE_OK
+        || (ret = curl_easy_setopt(curl, CURLOPT_WRITEDATA, chunk)) != CURLE_OK
+        || (ret =
+            curl_easy_setopt(curl, CURLOPT_USERAGENT, "Verse/1.0")) != CURLE_OK
+        || (ret =
+            curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L)) != CURLE_OK
+        || (ret = curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 5L)) != CURLE_OK) {
         return (int) ret;
     }
-    
-    return (int) curl_easy_perform (curl);
+
+    return (int) curl_easy_perform(curl);
 }
 
-int build_url (char **restrict url, const char *base_url, size_t base_url_size, int chapter, int verse)
+int build_url(char **restrict url, const char *base_url, size_t base_url_size,
+              int chapter, int verse)
 {
     /*
      * Allocate space for the full url. 
      */
-    *url = malloc (base_url_size * 2);
+    *url = malloc(base_url_size * 2);
 
     if (!(*url)) {
         return E_ENOMEM;
     }
-    snprintf (*url, base_url_size * 2,
-              "%s/%d:%d/en.maududi", base_url, chapter, verse);
+    snprintf(*url, base_url_size * 2,
+             "%s/%d:%d/en.maududi", base_url, chapter, verse);
     return E_SUCCESS;
 }
